1127698c38f1486b9e3547d19703057126686625
DongNV <dongnv@avada.email>
2025-04-01 16:53:38 +0700

    update

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,33 +1,46 @@
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
+import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+import {resolve} from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'LuckyCanvas',
-      formats: ['es'],
-      fileName: (format) => `index.${format === 'es' ? 'esm' : format}.js`
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "AvadaWheelCanvas",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format === "umd" ? "umd" : "esm"}.js`,
     },
     rollupOptions: {
       external: [],
       output: {
         globals: {
-          'lucky-canvas': 'LuckyCanvas'
-        }
-      }
+          "avada-wheel-canvas": "AvadaWheelCanvas",
+        },
+      },
     },
-    sourcemap: true
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
+    sourcemap: true,
+    target: "esnext",
+    chunkSizeWarningLimit: 1000,
   },
   plugins: [
     dts({
       insertTypesEntry: true,
-      include: ['src/**/*.ts']
-    })
+      include: ["src/**/*.ts"],
+    }),
   ],
   server: {
     port: 3002,
-    open: '/examples/wheel.html'
-  }
-}) 
+    open: "/examples/wheel.html",
+  },
+});
